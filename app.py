90d67be83356d9b2230d5064ca2af0534b19f9d5
Ernest Fudali <147720439+touljaboy@users.noreply.github.com>
2024-12-11 14:04:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    print('Request for index page received')
-   return "Hey bro"
+   return "Hey I hate you"
 
 @app.route('/favicon.ico')
 def favicon():
